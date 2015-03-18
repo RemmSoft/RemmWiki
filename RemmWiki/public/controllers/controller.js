9d69cb5f25f9882ad3587012a5deeb9407e7972b
Sevcan Alkan <sevcan.alkan@remmsoft.com>
2015-03-18 13:30:39 +0200

    #8 #6 #7 Proje siçimi eklendi, dökümanlar seçili projeye göre geliyor.

--- RemmWiki/public/controllers/controller.js
+++ RemmWiki/public/controllers/controller.js
@@ -4,38 +4,52 @@ rsWikiApp.controller('MainController',['$scope','$http',function($scope,$http){
 	console.log("LOG:Angular Controller Running. (MainController)");//
 
 	$scope.docList=null;
+	$scope.projectList=null;
 	$scope.selectedDoc=null;
 	$scope.orightml = '';
 	$scope.htmlcontent = $scope.orightml;
 	$scope.disabled = false;
 	$scope.currentProject = {
-		id : "54f3c008004a3e56ca765375",//
-		name : "LEAN",//
-		lang : "TR-tr",//
-		image : "...",//
+		_id : null,
+		name : "RemmSoft",
+		lang : "",
+		image : "",
 	};
 
-	var refresh = function () {
-		var projectId=$scope.currentProject.id;
+	var getProjects = function () {
+		$http.get('/getProjects').success(function(response){
+			console.log(response);
+			$scope.projectList=response;
+		});
+	};
 
-		$http.get('/getDocs/'+ projectId).success(function(response){
+	var refresh = function () {	
+		var projectId=$scope.currentProject._id;
+
+		if(projectId!=null){
+			$http.get('/getDocs/'+ projectId).success(function(response){
 				console.log("LOG:GET REQUEST Project Documents Success.");//
 				$scope.docList=response;
 				$scope.doc="";
 				$scope.selectedDoc=null;
 				$scope.orightml = '';
 				$scope.htmlcontent = $scope.orightml;
-				$scope.disabled = false;
+				$scope.disabled = false;				
 			});
-
+		}
 	};
 
-	refresh();
+	getProjects();
 
+	$scope.selectProject=function(project){				
+		$scope.currentProject=project;		
+		refresh();		
+	};
+	
 	$scope.addDoc = function(){
 		console.log($scope.doc);//
 
-		$scope.doc.projectId=$scope.currentProject.id;
+		$scope.doc.projectId=$scope.currentProject._id;
 
 		$http.post('/addDoc',$scope.doc).success(function (response) {
 			console.log(response);
@@ -71,12 +85,6 @@ rsWikiApp.controller('MainController',['$scope','$http',function($scope,$http){
 			refresh();
 		});
 	};
-/*
-	var wysiwygeditor = function () {
-		$scope.orightml = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li>Super Easy <b>Theming</b> Options</li><li style="color: green;">Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li class="text-danger">Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE8+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>';
-		$scope.htmlcontent = $scope.orightml;
-		$scope.disabled = false;
-	};*/
 
 }]);
 
